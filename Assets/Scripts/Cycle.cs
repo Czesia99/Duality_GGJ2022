842a99fed785fb4f1c3e62cd4040276d84e89418
yann <yann.berger@outlook.com>
2022-01-29 16:56:09 +0100

    background color change

---
+++ Assets/Scripts/Cycle.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cycle : MonoBehaviour
+{
+    public Camera cam;
+    public Color black = Color.black;
+    public Color white = Color.white; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam.backgroundColor = black;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (cam.backgroundColor == black)
+                cam.backgroundColor = white;
+            else
+                cam.backgroundColor = black;
+        }
+    }
+}
